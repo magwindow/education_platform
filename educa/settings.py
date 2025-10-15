3fa9b487bd5932475b7f78c79643b875616b6f8e
magwindow <antosha.petrakov@yandex.ru>
2025-10-15 18:22:14 +0300

    Now you can register a new student account and enroll in any course.

--- educa/settings.py
+++ educa/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -114,3 +115,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
