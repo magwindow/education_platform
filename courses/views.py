7a2a477f54980e9fce2d72305865199f2113a124
magwindow <antosha.petrakov@yandex.ru>
2025-10-15 17:34:16 +0300

    Authorized users are shown a button to enroll in the course. If it is unauthorized, a link to register on the platform is displayed.

--- courses/views.py
+++ courses/views.py
@@ -12,6 +12,7 @@ from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
+from students.forms import CourseEnrollForm
 
 
 class ManageCourseListView(ListView):
@@ -173,3 +174,8 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(initial={'course': self.object})
+        return context
