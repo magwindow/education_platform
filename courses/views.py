915caaf3e940c65099de4ae98907b1fbad20bf5e
magwindow <antosha.petrakov@yandex.ru>
2025-10-15 15:44:51 +0300

    A diverse content management system, rearranging modules and their contents by dragging and dropping

--- courses/views.py
+++ courses/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 from django.apps import apps
 from django.forms.models import modelform_factory
 from django.shortcuts import redirect, get_object_or_404
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 from .models import Course, Module, Content
 from .forms import ModuleFormSet
@@ -137,3 +138,17 @@ class ModuleContentListView(TemplateResponseMixin, View):
     def get(self, request, module_id):
         module = get_object_or_404(Module, id=module_id, course__owner=request.user)
         return self.render_to_response({'module': module})
+
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
